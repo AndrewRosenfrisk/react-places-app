7670674cb1a04c4ddfd784899f862ef6d7513bd6
Andrew <andrew.rosenfrisk@gmail.com>
2022-01-28 14:48:33 -0600

    get user data to display without styling

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-        return <h2>Users working</h2>;
+const USERS = [{
+        id: 'u1',
+        name: 'Andrew R.',
+        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.GhzpeXxemvti2aICJzVTEAHaEO%26pid%3DApi&f=1',
+        places: 3
+}]
+
+        return <UsersList items={USERS} />;
 }
 
 export default Users;
